a54056126a54b612847ce48448717dbeb51f6ae2
Artem Govorov <artem.govorov@gmail.com>
2018-02-10 00:23:16 +1000

    Initial commit

---
+++ test/files/file.test.js
@@ -0,0 +1,5 @@
+import l from '../../src/core/utils/logger';
+
+it('should work', () => {
+  console.log(l);
+});
---
+++ wallaby.js
@@ -0,0 +1,21 @@
+module.exports = function (wallaby) {
+  process.env.NODE_ENV = 'test';
+  return {
+    files: [
+      { pattern: 'src/**/*.js?(x)', load: false },
+      { pattern: 'test/fakeContexts.js', load: false }
+    ],
+    tests: [
+      'test/**/*.test.js'
+    ],
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    compilers: {
+      '**/*.js?(x)': wallaby.compilers.babel()
+    },
+    testFramework: 'jest',
+    debug: true
+  };
+};
